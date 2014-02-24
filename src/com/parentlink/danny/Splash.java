a7fb3da2bc98dd8dc27b5584e3c6ee76fd65fbae
Daniel North <dnorth2@hotmail.com>
2014-02-24 11:03:35 -0700

    Adding Splash Screen

---
+++ src/com/parentlink/danny/Splash.java
@@ -0,0 +1,32 @@
+package com.parentlink.danny;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Splash extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash);
+		Thread timer = new Thread(){
+			public void run(){
+				try{
+					sleep(5000);
+					
+				}catch (InterruptedException e){
+					e.printStackTrace();
+				}finally{
+					Intent openMainActivity= new Intent("com.parentlink.danny.MAINACTIVITY");
+					startActivity(openMainActivity);
+				}
+			}
+		};
+		timer.start();
+	}
+	
+	
+
+}
